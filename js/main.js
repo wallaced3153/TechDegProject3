e87e330f4bbf925d35e6659fdbdf78cc5a4ae60b
Derrick <derrickejwallace@gmail.com>
2018-06-21 21:00:21 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,24 +1,42 @@
+//On page load set focus to name text input and hide hideen text fields
+
 window.onload = function() {
     document.getElementById("name").focus();
-    document.getElementById("role_other_text").style.display = "none";
+    document.getElementById("other_text_container").style.display = "none";
 };
 
-var e = document.getElementById("title");
-var strUser = e.value;
-
+// var e = document.getElementById("title");
+// var strUser = e.value;
 
 
 $('#title').change(function(){ 
     var value = $(this).val();
     if (value === "other") {
-        // document.getElementById('role_other_text').style.display= "inline-flex";
-        // append.input()
-        // function myFunction() {
-        //     var x = document.createElement("INPUT");
-        //     x.setAttribute("type", "text");
-        //     x.setAttribute("value", "Hello World!");
-        //     document.body.appendChild(x);
-        // }
-        console.log(value);
-    }
+        document.getElementById("other_text_container").style.display = "block";
+    } else
+        document.getElementById("other_text_container").style.display = "none";
 });
+
+
+
+$('#design').change(function(){ 
+    var value = $(this).val();
+    console.log(value);
+    if (value == "js puns") {
+        var jsPuns = document.getElementById("color");
+        jsPuns.remove(jsPuns.value(tomato));
+       return$('$design');  
+    }     
+});
+
+// var design = document.getElementById("design");
+// var colors = document.getElementById("color"); 
+// for (var i = 0; i < op.length; i++) {
+//   // lowercase comparison for case-insensitivity
+//   (op[i].value.toLowerCase() == "js puns") 
+//     ? color.value(tomato).disabled = true 
+//     : op[i].disabled = false ;
+//     console.log(op);
+//     console.log(i);
+
+//}
