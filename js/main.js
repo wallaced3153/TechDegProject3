6f2789f4fc284626be8be88f0dd3286ce1d97d5c
Derrick <derrickejwallace@gmail.com>
2018-06-23 10:20:27 -0400

    update

--- js/main.js
+++ js/main.js
@@ -13,20 +13,48 @@ $('#title').change(function(){
     var value = $(this).val();
     if (value === "other") {
         document.getElementById("other_text_container").style.display = "block";
+        document.getElementById("other_title").focus();
     } else
         document.getElementById("other_text_container").style.display = "none";
 });
 
 
-
+//determine which option was selected for 'design'
 $('#design').change(function(){ 
     var value = $(this).val();
     console.log(value);
-    if (value == "js puns") {
-        var jsPuns = document.getElementById("color");
-        jsPuns.remove(jsPuns.value(tomato));
-       return$('$design');  
-    }     
+    //compare that to "js puns" then remove the div that does not 
+    //contain the colors for that design
+ if (value == "js puns") {
+        // var colorJS = document.getElementById("color_js");
+        $("#color option[value='tomato']").hide();
+        $("#color option[value='steelblue']").hide();
+        $("#color option[value='dimgrey']").hide();
+        $("#color option[value='cornflowerblue']").show();
+        $("#color option[value='darkslategrey']").show();
+        $("#color option[value='gold']").show();
+        return this.defaultSelected;
+    }
+ if (value == "heart js") {
+        // var colorJS = document.getElementById("color_js");
+        $("#color option[value='cornflowerblue']").hide();
+        $("#color option[value='darkslategrey']").hide();
+        $("#color option[value='gold']").hide();
+        $("#color option[value='tomato']").show();
+        $("#color option[value='steelblue']").show();
+        $("#color option[value='dimgrey']").show();
+        return this.defaultSelected;
+  } 
+  if (value == "default") {
+        $("#color option[value='tomato']").show();
+        $("#color option[value='steelblue']").show();
+        $("#color option[value='dimgrey']").show();
+        $("#color option[value='cornflowerblue']").show();
+        $("#color option[value='darkslategrey']").show();
+        $("#color option[value='gold']").show();
+        return this.defaultSelected;
+    }
+
 });
 
 // var design = document.getElementById("design");
